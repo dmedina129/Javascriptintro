5837e03ec360c9c8ec4b2f3f09e41ee02d35f1c1
Daniela Medina <daniela@Danielas-MacBook-Pro.local>
2023-03-12 12:17:38 -0400

    first comit

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = " Daniela ";
+var lastName = "Medina ";
+alert("My name is" +  firstName + lastName );
+
+var age = " 27";
+alert("I am" + age + " years old");
+
+console.log("I am having so much fun in this class");
